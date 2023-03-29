ae1ffdd9d6a65dc6961a5c4679fd7cf323fa168a
+ <+>
2023-03-29 22:15:45 +0300

    palindrome

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
+// const chai = require("chai");
+// global.expect = chai.expect;
+// const isPalindrome = require("../index");
+
+// describe("isPalindrome", () => {
+//   it("returns true for 'abba'", () => {
+//     expect(isPalindrome("abba")).to.be.true;
+//   });
+//   it("returns true for 'racecar'", () => {
+//     expect(isPalindrome("racecar")).to.be.true;
+//   });
+//   it("returns true for 'a'", () => {
+//     expect(isPalindrome("a")).to.be.true;
+//   });
+//   it("returns false for 'robot'", () => {
+//     expect(isPalindrome("robot")).to.be.false;
+//   });
+//   it("returns false for 'ab'", () => {
+//     expect(isPalindrome("ab")).to.be.false;
+//   });
+// });
+
+
+// reverce method to return our string in reverse order
+function reverse(word){
+
+  const wordArray= word.split("")
+  const reversedWordArr = wordArray.reverse()
+  const name =reversedWordArr.join("")
+  return name;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm her
+
+  const name = reverse(word)
+
+    if (word === name){
+      return true
+    }
+  else{
+    return false 
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  if input is = reversed inpu
+  return true
+
+  else
+  false
 */
 
 /*
